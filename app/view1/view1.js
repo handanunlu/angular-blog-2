6821d424e1566d2aba2cc1130cba151abf2bb55e
handan_unlu <handanunluu@bitbucket.org>
2018-11-19 11:40:32 +0300

    First commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,26 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+ $routeProvider.when('/view1', {
+   templateUrl: 'view1/view1.html',
+   controller: 'View1Ctrl'
+ });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+ $scope.list = [{"id":5,"title":"How To Use It?","content":"Please add some content or make come comments to other contents. And also please have fun!","comment":"","newComment":""}];
+ localStorage.setItem('posts', JSON.stringify($scope.list));
+ $scope.list = JSON.parse(localStorage.getItem('posts'));
+ $scope.addComment = function (data) {
+     angular.forEach($scope.list, function (v) {
+         if (v.id === data.id) {
+           console.log(v.comment);
+           v.comment.push({text: v.newComment});
+           v.newComment = "";
+         }
+     });
+     localStorage.setItem('posts', JSON.stringify($scope.list));
+     $scope.list = JSON.parse(localStorage.getItem('posts'));
+ }
 
 }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,12 +3,17 @@
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+ $routeProvider.when('/view2', {
+   templateUrl: 'view2/view2.html',
+   controller: 'View2Ctrl'
+ });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+ $scope.allPosts = JSON.parse(localStorage.getItem('posts'));
+ $scope.data = {id: $scope.allPosts[$scope.allPosts.length -1].id + 1, title: "", content: "", comment: [], newComment: ""};
+ $scope.addPost = function () {
+     $scope.allPosts.push($scope.data);
+     localStorage.setItem('posts', JSON.stringify($scope.allPosts))
+ }
 }]);
